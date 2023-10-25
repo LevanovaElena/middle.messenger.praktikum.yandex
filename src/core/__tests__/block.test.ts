e09aa3176d970fa9a3f812057887d533e7e167c8
ellevanova <ellevanova@gmail.com>
2023-10-25 22:05:05 +0400

    added tests

--- src/core/__tests__/block.test.ts
+++ src/core/__tests__/block.test.ts
@@ -1,24 +1,38 @@
 import Block from "../block.ts";
-import {assert} from "chai";
-//import {expect} from "chai";
+import { expect} from "chai";
+import sinon from "sinon";
 
 describe('Block', () => {
-    it('Create Block is correct', () => {
 
-        const block=new Block({});
-        assert.equal(block.getContent(),null)
+    class Component extends Block {
+        constructor(props :{text:string,events:{}} ) {
+            super(props);
+        }
+        public get props() {
+            return this._props as {text:string,events:{}};
+        }
+        render(): string {
+            return '<div></div>'
+        }
+    }
+    it('Create Component is correct', () => {
+
+        const block=new Component({text:'text',events:{}});
+        expect(block.getContent()).not.null;
     });
+    it('Component should render', () => {
 
-   /* describe('Проверка авторизации через форму', () => {
-        it('Проверяем валидные данные в signin', () => {
-        });
-
-        it('Проверяем валидные данные в signup', () => {
-        });
+        const block=new Component({text:'text',events:{}});
+       const render= sinon.spy(block,'render');
+        expect(render.calledOnce);
     });
+    it('Component should render again after change props', () => {
 
-    describe('Дополнительный функционал при авторизации', () => {
-        it('Что-то дополнительно связанное с авторизацией', () => {
-        });
-    });*/
+        const block=new Component({text:'text',events:{}});
+        const render= sinon.spy(block,'render');
+        expect(render.calledOnce);
+        block.props.text='new text';
+        block.setProps( block.props);
+        expect(render.called);
+    });
 });
